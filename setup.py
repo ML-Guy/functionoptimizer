f947ffbca2d900af7ae9e54df0a36d855b52b605
Saurabh Yadav <saurabh5228@gmail.com>
2017-12-26 19:17:47 +0530

    setup.py: installing saurabhy/hermes via os.system instead of install_requires

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
+import os
+os.system('pip install hg+https://bitbucket.org/saurabhy/hermes')
+
 setup(name='functionoptimizer',
       version='0.1.0',
       description='Optimization library for functions. It aggregates available optimisations tools.',
       url='https://github.com/ML-Guy/functionoptimizer',
       install_requires=[
           'six',
-          'numba',
-          r'hg+https://bitbucket.org/saurabhy/hermes' ],
+          'numba',],
       packages=['functionoptimizer'],
       zip_safe=False)
